533eea11e033bbaf3989205ef9faec4b0584cc16
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-04-04 08:53:01 -0300

    add- 1

---
+++ script.js
@@ -0,0 +1,17 @@
+ function soma (numero){
+ let total= 0;
+
+ for let i=0, i<0, i++{
+    let valor = parseFloat(prompt(` digite o ${ i +1} número:`));
+    total += valor 
+ 
+ }
+
+ let quantidade= parseInt(prompt("Quantos números voce quer somar"?));
+ let total= quantidade(soma);
+ console.log(" o resultado final é:", resultado);
+ 
+
+
+
+ 
