1ae0c9e791a0764f9f203962914427b0f0af73b9
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-04-04 08:55:50 -0300

    add-

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
- function soma (numero){
+ function soma (numero) {
  let total= 0;
 
- for let i=0, i<0, i++{
+ for (let i=0, i<0, i++){
     let valor = parseFloat(prompt(` digite o ${ i +1} número:`));
     total += valor;
  }
 
- let quantidade= parseInt(prompt("Quantos números voce quer somar"?));
- let total= quantidade(soma);
+ let quantidade = parseInt(prompt("Quantos números voce quer somar"?));
+ let total = quantidade(soma);
  console.log(" o resultado final é:", resultado);
 
 
